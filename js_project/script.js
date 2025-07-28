81d0148ee3ef612da69840b3dd28ac0961e7a0d8
Ajithkumar <ajithkumar.rajendran08@gmail.com>
2025-07-28 14:47:13 +0530

    new add

--- js_project/script.js
+++ js_project/script.js
@@ -1,5 +1,10 @@
 let formContainer= document.createElement("div");
 let mainContainer= document.getElementById("main");
+let listContainer = document.createElement("div");
+listContainer.setAttribute(
+  "class",
+  "flex justify-start items-center p-4 flex-wrap gap-2"
+);
 formContainer.setAttribute("id","form-box");
 formContainer.innerHTML = `
 <form class="shadow-xl w-[700px] h-[400px] flex justify-center items-center flex-col gap-3">
@@ -11,7 +16,7 @@ formContainer.innerHTML = `
 </form?
 `;
 
-mainContainer.append(formContainer);
+mainContainer.append(formContainer , listContainer);
 let nameInput = document.getElementById("name-input");
 let batchInput = document.getElementById("batch-input");
 let courseInput = document.getElementById("course-input");
@@ -22,6 +27,7 @@ function removeInputField(){
     batchInput.value = "";
     courseInput.value = "";
 }
+let student_list = [];
 formContainer.addEventListener("submit",(e)=>{
     e.preventDefault();
     const newStudentObj = {
@@ -31,8 +37,34 @@ formContainer.addEventListener("submit",(e)=>{
     };
     if(newStudentObj){
         console.log(newStudentObj);
+        student_list.push(newStudentObj);
         removeInputField();
+        createStudentCard(newStudentObj);
     }
 });
 
+const dummyObj = {
+    student_name: "Sanjay",
+    student_batch: "B29",
+    student_course: "Full Stack",
+};
+function createStudentCard(studentObj){
+    listContainer.innerHTML += ` 
+                    <div class="bg-slate-100 p-4 rounded-lg w-[150px] h-[150px] flex flex-col justify-between gap-2 shadow-md">
+                    <div class="text-2xl text-center font-bold">${studentObj.student_name}</div>
+                    <div>
+                    <div class="opacity-75 font-semibold">${studentObj.student_batch}</div>
+                    <div class="opacity-75 font-semibold">${studentObj.student_course}</div>
+                    </div>
+                    </div> 
+                    `;
+}
 
+function renderAllStudentsCard(studenList) {
+  studenList.map((stud_obj, index, accARR) => {
+    createStudentCard(stud_obj);
+  });
+}
+if (student_list.length > 0) {
+  renderAllStudentsCard(student_list);
+}
